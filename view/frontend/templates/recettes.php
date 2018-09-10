159648b2594b3774b04b6f95191112dcfd4d2ed5
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-09-10 10:15:47 +0700

    resolve error link

--- view/frontend/templates/recettes.php
+++ view/frontend/templates/recettes.php
@@ -12,10 +12,10 @@
 <?php foreach (array_slice($recettes, 0, 3) as $recette) : ?>
 						<div class="col-md-4 animate-box">
 							<article class="article-entry">
-								<a href="index.php?page=uneRecette&amp;recetteId=<?= $recette['id'] ?>" class="blog-img" style="background-image: url(<?= $recette['recette_img'] ?>);"></a>
+								<a href="../../../index.php?page=uneRecette&amp;recetteId=<?= $recette['id'] ?>" class="blog-img" style="background-image: url(<?= $recette['recette_img'] ?>);"></a>
 								<div class="desc" style="padding-left: 20px;">
 									<p class="admin"><span>Ecrit par :</span> <span><?= htmlspecialchars($recette['recette_author']) ?></span></p>
-									<h2><a href="index.php?page=uneRecette&amp;recetteId=<?= $recette['id'] ?>"><?= htmlspecialchars($recette['recette_title']) ?></a></h2>
+									<h2><a href="../../../index.php?page=uneRecette&amp;recetteId=<?= $recette['id'] ?>"><?= htmlspecialchars($recette['recette_title']) ?></a></h2>
 									<p><?= htmlspecialchars($recette['recette_subtitle']) ?></p>
 								</div>
 							</article>
